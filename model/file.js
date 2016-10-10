1e45d854fe1cf3e2defa3d9bc43b2f887872d728
kaylynyuh <yuhkay4@gmail.com>
2016-10-10 09:36:04 -0700

    change profile-pic.js to file.js, finished fileSchema

---
+++ model/file.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const fileSchema = Schema({
+  userID: {type: mongoose.Schema.Types.ObjectId, required: true},
+  fileURI: {type: String, required: true, unique: true},
+  objectKey: {type: String, required: true, unique: true},
+  fileType: {type: String, required: true},
+  created: {type: Date, default: Date.now},
+});
