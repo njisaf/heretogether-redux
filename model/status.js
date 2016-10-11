dc32a61adebb38f6871734054687d84d8e7aef01
kaylynyuh <yuhkay4@gmail.com>
2016-10-11 15:52:58 -0700

    added changes to status schema, buidling out file router

--- model/status.js
+++ model/status.js
@@ -6,13 +6,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const statusSchema = Schema({
-  userID: {type: String, required:true, unique:true},
+  userID: {type: String, required:true},
   text: {type: String},
-  fileID: {type: mongoose.Schema.Types.ObjectId, required: true},
-  picID: {type: mongoose.Schema.Types.ObjectId, required: true},
+  fileID: {type: mongoose.Schema.Types.ObjectId},
+  picID: {type: mongoose.Schema.Types.ObjectId},
   replyTo: {type: String},
-  username: {type: String},
-  hospitalID: {type: String},
+  username: {type: String, required: true},
+  hospitalID: {type: mongoose.Schema.Types.ObjectId, required: true},
   created: {type: Date, default: Date.now},
 });
 
