66e4e5398caf710a52c0c41fb2732eceac66959e
kaylynyuh <yuhkay4@gmail.com>
2016-10-11 09:44:38 -0700

    started POST route /api/profile

---
+++ route/profile-router.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Router = require('express').Router;
+const debug = require('debug')('ht:hospital-router');
+const jsonParser = require('body-parser').json();
+const bearerAuth = require('../lib/bearer-auth-middleware');
+const Profile = require('../model/profile');
+
+const profileRouter = module.exports = Router();
+
+profileRouter.post('/api/hospital/hospitalID/profile', bearerAuth, jsonParser, function(req, res, next){
+  debug('hit POST route /api/hospital/hospitalID/profile');
+});
