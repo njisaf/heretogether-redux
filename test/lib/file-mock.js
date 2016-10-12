75c890e55176cd2847761693beeb6e6718ddf541
kaylynyuh <yuhkay4@gmail.com>
2016-10-12 09:16:40 -0700

    fileMock added, moving onto file-router tests

--- test/lib/file-mock.js
+++ test/lib/file-mock.js
@@ -2,7 +2,7 @@
 
 const debug = require('debug')('ht:file-mock');
 const File = require('../../model/file');
-// const awsMock = require('./aws-mock')
+const awsMocks = require('./aws-mocks');
 const profileMock = require('./profile-mock');
 
 module.exports = function(done){
@@ -11,19 +11,18 @@ module.exports = function(done){
     name: 'Tom',
     desc: 'heir of slytherin',
     created: new Date(),
-    // imageURI: awsMocks.uploadMock.Location,
-    // objectKey: awsMocks.uploadMock.Key,
+    imageURI: awsMocks.uploadMock.Location,
+    objectKey: awsMocks.uploadMock.Key,
   };
+  profileMock.call(this, err => {
+    if (err) return done(err);
+    exampleFileData.userID = this.tempUser._id.toString();
+    exampleFileData.profileID = this.tempProfile._id.toString();
+    new File(exampleFileData).save()
+    .then( file => {
+      this.tempFile = file;
+      done();
+    })
+    .catch(done);
+  });
 };
-
-profileMock.call(this, err => {
-  if (err) return done(err);
-  exampleFileData.userID = this.tempUser._id.toString();
-  exampleFileData.profileID = this.tempProfile._id.toString();
-  new File(exampleFileData).save()
-  .then( file => {
-    this.tempFile = file;
-    done();
-  })
-  .catch(done);
-});
