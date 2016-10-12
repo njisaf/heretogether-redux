1d6a7652fdb5f42a1053cb4cd2e4a1247616d645
kaylynyuh <yuhkay4@gmail.com>
2016-10-12 09:07:20 -0700

    commiting changes to branch so Judy can merge into this branch. need aws-mock

---
+++ test/lib/file-mock.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const debug = require('debug')('ht:file-mock');
+const File = require('../../model/file');
+// const awsMock = require('./aws-mock')
+const profileMock = require('./profile-mock');
+
+module.exports = function(done){
+  debug('creating mock file');
+  let exampleFileData = {
+    name: 'Tom',
+    desc: 'heir of slytherin',
+    created: new Date(),
+    // imageURI: awsMocks.uploadMock.Location,
+    // objectKey: awsMocks.uploadMock.Key,
+  };
+};
+
+profileMock.call(this, err => {
+  if (err) return done(err);
+  exampleFileData.userID = this.tempUser._id.toString();
+  exampleFileData.profileID = this.tempProfile._id.toString();
+  new File(exampleFileData).save()
+  .then( file => {
+    this.tempFile = file;
+    done();
+  })
+  .catch(done);
+});
