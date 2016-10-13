b04c7224e4f7d36d95eed3c78d36c05e0ec8375d
JudyVue <judyvue87@gmail.com>
2016-10-12 18:51:28 -0700

    'Testing PUT with NO TOKEN for status 400

--- test/status-router-test.js
+++ test/status-router-test.js
@@ -305,6 +305,20 @@ describe('Testing Status routes', function() {
         });
       });
     });
+
+    describe('Testing PUT with NO TOKEN', function() {
+
+      before(done => mockStatus.call(this, done));
+
+      it('Should return a status of 400 for no token', done => {
+        request.put(`${url}/api/hospital/${this.tempHospital._id}/status/${this.tempStatus._id}`)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(err.message).to.equal('Bad Request');
+          done();
+        });
+      });
+    });
   });
 
 
