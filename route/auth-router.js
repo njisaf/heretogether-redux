bbe939256eb5b62de07477023323de24e63dcd26
JudyVue <judyvue87@gmail.com>
2016-11-14 09:55:31 -0800

    added Google-o-auth-middleware and added extra oauth callback route in auth-router

--- route/auth-router.js
+++ route/auth-router.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const googleOAUTH = require('../lib/google-oauth-middleware.js');
 const Router = require('express').Router;
 const debug = require('debug')('ht:auth-router');
 const jsonParser = require('body-parser').json();
@@ -37,4 +38,47 @@ authRouter.get('/api/login', basicAuth, function(req, res, next) {
   .then(user => user.generateToken())
   .then(token => res.send(token))
   .catch(next);
+});
+
+authRouter.get('/api/auth/oauth_callback', googleOAUTH, function(req, res){
+  // should have either req.googleError or req.googleOAUTH
+  console.log('googleError', req.googleError);
+  console.log('googleOAUTH', req.googleOAUTH);
+
+  // if googleError deal with google error
+  if(req.googleError){
+    return res.redirect('/#/home');
+  }
+  // check if user already exists
+  User.findOne({email: req.googleOAUTH.email})
+  .then( user => {
+    if (!user) return Promise.reject(new Error('user not found'));
+    return user;
+  })
+  .catch(err => {
+    if(err.message === 'user not found'){
+      let userData = {
+        username: req.googleOAUTH.email,
+        email: req.googleOAUTH.email,
+        google: {
+          googleID: req.googleOUATH.googleID,
+          tokenTimeToLive: req.googleOUATH.tokenTimeToLive,
+          tokenTimestamp: Date.now(),
+          refreshToken: req.googleOUATH.refreshToken,
+          accessToken: req.googleOUATH.accessToken,
+        },
+      };
+      return new User(userData).save();
+    }
+    return Promise.reject(err);
+  })
+  .then( user => user.generateToken())
+  .then(token => {
+    res.redirect(`/?token=${token}`);
+  })
+  .catch(err => {
+    console.err(err);
+    console.log('user not found');
+    res.redirect('/#/home');
+  });
 });
