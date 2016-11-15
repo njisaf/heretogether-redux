765a3116cbcdffd88224eed068e9e526b8e36124
JudyVue <judyvue87@gmail.com>
2016-11-14 16:54:41 -0800

    trying to get signupCTRL test working but failing still

--- client-test/signup-controller-test.js
+++ client-test/signup-controller-test.js
@@ -6,13 +6,15 @@
 //
 //   beforeEach(() => {
 //     angular.mock.module(camelcase(__TITLE__));
-//     angular.mock.inject(($rootScope, $location, $httpBackend, $componentController, authService) => {
-//       authService.setToken('1234');
+//     angular.mock.inject(($rootScope, $location, $httpBackend, $componentController, authService, profileService, hospitalService) => {
+//       // authService.setToken('1234');
 //
 //       this.$location = $location;
 //       this.$rootScope = $rootScope;
 //       this.$httpBackend = $httpBackend;
 //       this.authService = authService;
+//       this.profileService = profileService;
+//       this.hospitalService = hospitalService;
 //       this.$componentController = $componentController;
 //     });
 //   });
@@ -22,6 +24,8 @@
 //   });
 //
 //   it('testing signup controller', () => {
+//
+//
 //     let user = {
 //       username: 'mockUserName',
 //       email: 'mockEmail',
@@ -31,16 +35,16 @@
 //     let profile = {
 //       profileName: 'mockProfileName',
 //       userID: '1234FIVE',
-//       hospitalID: '6789TEN',
+//       hospitalID: 'mockHospitalID',
 //     };
 //
 //     let hospital = {
-//       name: 'mockHospitalName',
+//       name: 'Seattle Children\'s Hospital',
 //     };
 //
 //     let signupURL = 'http://localhost:3000/api/signup';
 //
-//     let profileURL = 'http://localhost:3000/api/6789TEN/profile';
+//     let profileURL = 'http://localhost:3000/api/hospital/mockHospitalID/profile';
 //
 //     let hospitalURL = 'http://localhost:3000/api/hospital';
 //
@@ -48,11 +52,15 @@
 //       Accept: 'application/json',
 //       'Content-Type': 'application/json',
 //     };
-//     this.$httpBackend.expectPOST(hospitalURL, hospital, headers)
-//     .respond(200, {_id:'1234567890', name: 'mockHospitalName', created: Date.now()});
 //
-//     this.$httpBackend.expectPOST(signupURL, user, headers)
+//     this.$httpBackend.expectPOST(hospitalURL, hospital, headers)
+//     .respond(200, {_id:'1234567890', name: hospital.name, created: Date.now()});
+//
+//     // this.hospitalService.hospitalID = 'mockHospitalID';
+//
+//     this.$httpBackend.whenPOST(signupURL, user, headers)
 //     .respond(200, {_id: '1234FIVE', username: 'mockUserName', email: 'mockEmail', password: 'mockPassword'});
+//
 //
 //     this.$httpBackend.expectPOST(profileURL, profile, headers)
 //     .respond(200, {_id:'1234567890', userID: '1234FIVE', profileName: 'mockProfileName', hospitalID: '6789TEN'});
@@ -63,6 +71,7 @@
 //     signupCtrl.signup(user);
 //
 //     this.$httpBackend.flush();
+//     console.log('hahahahaha', this.hospitalService.hospitalID);
 //
 //     expect(this.$location.path()).toBe('/home');
 //
