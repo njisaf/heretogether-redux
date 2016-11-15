120f023270455a16acbde82023fa523653476bc3
JudyVue <judyvue87@gmail.com>
2016-11-15 14:58:39 -0800

    got rid of fileService in uploadPicCtrl

--- app/component/upload-file/upload-file.js
+++ app/component/upload-file/upload-file.js
@@ -10,7 +10,7 @@ module.exports = {
   },
 };
 
-function UploadFileController($log, fileService){
+function UploadFileController($log){
   $log.debug('Initializing uploadFileCtrl');
 
   this.file = {};
