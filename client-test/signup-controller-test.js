f979ff12743fc3d69a5e094cf3543b5753406efc
JudyVue <judyvue87@gmail.com>
2016-11-15 09:09:01 -0800

    still trying to figure out test

--- client-test/signup-controller-test.js
+++ client-test/signup-controller-test.js
@@ -7,7 +7,7 @@
 //   beforeEach(() => {
 //     angular.mock.module(camelcase(__TITLE__));
 //     angular.mock.inject(($rootScope, $location, $httpBackend, $componentController, authService, profileService, hospitalService) => {
-//       // authService.setToken('1234');
+//       authService.setToken('1234');
 //
 //       this.$location = $location;
 //       this.$rootScope = $rootScope;
@@ -33,8 +33,8 @@
 //     };
 //
 //     let profile = {
-//       profileName: 'mockProfileName',
-//       userID: '1234FIVE',
+//       profileName: user.username,
+//       userID: user._id,
 //       hospitalID: 'mockHospitalID',
 //     };
 //
@@ -48,22 +48,29 @@
 //
 //     let hospitalURL = 'http://localhost:3000/api/hospital';
 //
-//     let headers = {
+//     let headersWithToken = {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json',
+//       Authorization: 'Bearer 1234',
+//     };
+//
+//     let headersNoToken = {
 //       Accept: 'application/json',
 //       'Content-Type': 'application/json',
 //     };
 //
-//     this.$httpBackend.expectPOST(hospitalURL, hospital, headers)
-//     .respond(200, {_id:'1234567890', name: hospital.name, created: Date.now()});
-//
-//     // this.hospitalService.hospitalID = 'mockHospitalID';
-//
-//     this.$httpBackend.whenPOST(signupURL, user, headers)
+//     this.$httpBackend.whenPOST(signupURL, user, headersNoToken)
 //     .respond(200, {_id: '1234FIVE', username: 'mockUserName', email: 'mockEmail', password: 'mockPassword'});
 //
+//     this.$httpBackend.expectPOST(hospitalURL, hospital, headersWithToken)
+//     .respond(200, {_id:'1234567890', name: hospital.name, created: Date.now()});
 //
-//     this.$httpBackend.expectPOST(profileURL, profile, headers)
-//     .respond(200, {_id:'1234567890', userID: '1234FIVE', profileName: 'mockProfileName', hospitalID: '6789TEN'});
+//     this.hospitalService.hospitalID = 'mockHospitalID';
+//
+//
+//
+//     this.$httpBackend.expectPOST(profileURL, profile, headersWithToken)
+//     .respond(200, {_id: user._id, profileName: user.username, userID: profile.userID,  hospitalID: this.hospitalService.hospitalID});
 //
 //
 //     let signupCtrl = this.$componentController('signup');
