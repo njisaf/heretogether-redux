2d4c6b7ff6390f0ad68c82deea1c52b10baee287
JudyVue <judyvue87@gmail.com>
2016-11-15 14:51:24 -0800

    upload files added

---
+++ app/component/upload-file/upload-file.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+module.exports = {
+  controllerAs: 'uploadFileCtrl',
+  template: require('./upload-file.html'),
+  controller: ['$log', 'fileService', UploadFileController],
+  bindings: {
+    status: '=',
+  },
+};
+
+function UploadFileController($log, fileService){
+  $log.debug('Initializing uploadFileCtrl');
+
+  this.file = {};
+
+  this.uploadStatusFile = function(){
+    console.log('UploadFileController.uploadStatusFile');
+    this.status.file = this.file.file;
+    console.log('this.status.file', this.status.file);
+    this.file.file = null;
+
+  };
+
+
+}
