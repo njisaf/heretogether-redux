d46fea4459cc886f86f3ddd6b2dc487c52714606
Nassir Isaf <njisaf@gmail.com>
2016-11-15 21:21:16 -0800

    fetchProfiles and deleteProfile

--- app/service/profile-service.js
+++ app/service/profile-service.js
@@ -66,11 +66,56 @@ function profileService($q, $log, $http, authService, hospitalService) {
   };
 
   profileService.fetchProfiles = function() {
-    $log.debug('Hit profileService.fetchProfiles. Fetching ALL profiles');
+    $log.debug('Hit profileService.fetchProfiles. Fetching all profiles');
 
     return authService.getToken()
-    
-  }
+    .then(token => {
+      let url = `${__API_URL__}/api/hospital/${hospitalService.hospitalID}/profile/all`;
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      return $http.get(url, config);
+    })
+    .then(res => {
+      $log.log('Profiles fetched');
+      service.profiles = res.data;
+      return service.profiles;
+    })
+    .catch(err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  profileService.deleteProfile = function(profileID) {
+    $log.debug('Hit profileService.deleteProfile');
+
+    return authService.getToken()
+    .then(token => {
+      let url = `${__API_URL__}/api/hospital/${hospitalService.hospitalID}/status/${profileID}`;
+      let config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      return $http.delete(url, config);
+    })
+    .then(() => {
+      $log.log('profile deleted');
+      for (var i = 0; i < service.profiles.length; ++i) {
+        if (profileID === service.profiles[i]._id) service.profiles.splice(i, 1);
+      }
+      return service.profiles;
+    })
+    .catch(err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
 
   return service;
 
