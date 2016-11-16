fe369e0d00d605b403b09d20b6e7753ad6fe8b59
Nassir Isaf <njisaf@gmail.com>
2016-11-16 15:06:11 -0800

    navigating to individual profiles from profile feed

---
+++ app/view/show-profile/show-profile-controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('./show-profile.html');
+
+module.exports = ['$log', ShowProfileController];
+
+function ShowProfileController($log) {
+  $log.debug('Initializing ShowProfileController');
+
+
+}
