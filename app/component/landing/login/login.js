eab439be1b27bf59831b3e08dc9cbf950c44bb2d
JudyVue <judyvue87@gmail.com>
2016-11-17 13:34:21 -0800

    got dropdown menu to appear

--- app/component/landing/login/login.js
+++ app/component/landing/login/login.js
@@ -3,12 +3,15 @@
 
 module.exports = {
   template: require('./login.html'),
-  controller: ['$log', '$location', 'authService', LoginController],
+  controller: ['$log', '$location', 'authService', 'hospitalService', LoginController],
   controllerAs: 'loginCtrl',
 };
 
-function LoginController($log, $location, authService){
+function LoginController($log, $location, authService, hospitalService){
+
+  this.hospitals = [];
   this.login = function(user){
+    console.log('hospital', this.hospital);
     authService.login(user)
     .then(() => {
       $location.path('/home'); // upon successful login, route user to homepage
@@ -17,4 +20,14 @@ function LoginController($log, $location, authService){
       console.log('failed to login');
     });
   };
+
+  this.getHospitalAtLogin = function(){
+    hospitalService.fetchHospitals()
+    .then((hospitals) => {
+      this.hospitals = hospitals.data;
+      return this.hospitals;
+    });
+  };
+
+  this.getHospitalAtLogin();
 }
