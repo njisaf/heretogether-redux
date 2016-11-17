9284d1cf597c458e2090e98d24918992d4a452ad
JudyVue <judyvue87@gmail.com>
2016-11-17 11:21:41 -0800

    got nav bar to go to profile page

--- app/component/footer/footer.js
+++ app/component/footer/footer.js
@@ -4,11 +4,11 @@ require('./_footer.scss');
 
 module.exports = {
   template: require('./footer.html'),
-  controller: ['$log', '$location', '$rootScope', 'authService', FooterController],
+  controller: ['$log', '$location', '$rootScope', 'authService', 'profileService', FooterController],
   controllerAs: 'footerCtrl',
 };
 
-function FooterController($log, $location, $rootScope, authService){
+function FooterController($log, $location, $rootScope, authService, profileService){
   $log.debug('init FooterController');
 
   this.checkPath = function(){
@@ -37,6 +37,19 @@ function FooterController($log, $location, $rootScope, authService){
   };
 
   this.checkPath();
+
+  //Judy and Nassir's additions
+
+  this.goToUserProfile = function(){
+
+    profileService.getOneProfileNoID()
+    .then((profile) => {
+      $location.url(`/profile/id=${profile._id}`);
+    })
+    .catch((err) => {
+      $log.error(err.message);
+    });
+  };
 
   $rootScope.$on('$locationChangeSuccess', () => {
     this.checkPath();
