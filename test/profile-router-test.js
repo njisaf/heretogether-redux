6197b6c5ca063cfb8bf12c33e326f2850a8144ff
Nassir Isaf <njisaf@gmail.com>
2017-02-08 12:01:06 -0800

    going to break here so I don't have to do this all on one big dumb branch

--- test/profile-router-test.js
+++ test/profile-router-test.js
@@ -25,12 +25,16 @@ const exampleProfile = {
   bio: 'Bio',
 };
 
-
 describe('Testing Profile routes', function() {
 
   before(done => serverCtrl.serverUp(server, done));
   after(done => serverCtrl.serverDown(server, done));
   afterEach(done => cleanDB(done));
+
+  // 'Testing POST /api/profile'
+  //  'Testing POST with VALID BODY'
+  //  'Testing POST with INVALID BODY'
+  //  'Testing POST with VALID BODY but NO AUTHORIZATION'
 
   describe('Testing POST /api/profile', function() {
 
@@ -93,151 +97,18 @@ describe('Testing Profile routes', function() {
 
   });
 
+  describe('Testing /api/profile/:profileID GET', function() {
+
+    describe('Testing GET with VALID profileID', function() {
+
+      before(done => )
+
+    });
+
+  });
+
 });
 
-//
-// describe('Testing Profile routes', function() {
-//
-//   before(done => serverCtrl.serverUp(server, done));
-//   after(done => serverCtrl.serverDown(server, done));
-//   afterEach(done => cleanDB(done));
-//
-//   describe('Testing POST /api/hospital/:hospitalID/profile', function() {
-//
-//     describe('Testing POST with VALID hospitalID and VALID BODY', function() {
-//
-//       before(done => mockUser.call(this, done));
-//       before(done => mockHospital.call(this, done));
-//
-//       it('Should return a status of 200 and a profile', done => {
-//         request.post(`${url}/api/hospital/${this.tempHospital._id}/profile`)
-//         .send({
-//           profileName: exampleProfile.profileName,
-//           userID: `${this.tempUser._id}`,
-//           bio: exampleProfile.bio,
-//           hospitalID: `${this.tempHospital._id}`,
-//         })
-//         .set({Authorization: `Bearer ${this.tempToken}`})
-//         .end((err, res) => {
-//           if(err) return done(err);
-//           expect(res.status).to.equal(200);
-//           expect(res.body.profileName).to.equal(exampleProfile.profileName);
-//           expect(res.body.bio).to.equal(exampleProfile.bio);
-//           done();
-//         });
-//       });
-//     });
-//
-//     describe('Testing POST with VALID IDs but NO HOSPITAL', function() {
-//
-//       before(done => mockUser.call(this, done));
-//       before(done => mockFakeHospital.call(this, done));
-//
-//       it('Should return a status of 404 and an error message', done => {
-//         request.post(`${url}/api/hospital/${this.tempHospital._id}/profile`)
-//         .send({
-//           profileName: exampleProfile.profileName,
-//           userID: `${this.tempUser._id}`,
-//           bio: exampleProfile.bio,
-//           hospitalID: `${this.tempHospital._id}`,
-//         })
-//         .set({Authorization: `Bearer ${this.tempToken}`})
-//         .end((err, res) => {
-//           expect(res.status).to.equal(404);
-//           expect(res.text).to.equal('NotFoundError');
-//           done();
-//         });
-//       });
-//     });
-//
-//
-//     describe('Testing POST with VALID hospitalID, VALID BODY, but NO AUTHORIZATION', function() {
-//
-//       before(done => mockUser.call(this, done));
-//       before(done => mockHospital.call(this, done));
-//
-//       it('Should return a status of 400 and an error message', done => {
-//         request.post(`${url}/api/hospital/${this.tempHospital._id}/profile`)
-//         .send({
-//           profileName: exampleProfile.profileName,
-//           userID: `${this.tempUser._id}`,
-//           bio: exampleProfile.bio,
-//           hospitalID: `${this.tempHospital._id}`,
-//         })
-//         .end((err, res) => {
-//           expect(res.status).to.equal(400);
-//           expect(res.text).to.equal('BadRequestError');
-//           done();
-//         });
-//       });
-//     });
-//
-//
-//     describe('Testing POST with VALID hospitalID and INVALID BODY', function() {
-//
-//       before(done => mockUser.call(this, done));
-//       before(done => mockHospital.call(this, done));
-//
-//       it('Should return a status of 400 and an error message', done => {
-//         request.post(`${url}/api/hospital/${this.tempHospital._id}/profile`)
-//         .set('Content-type', 'application/json')
-//         .send('This is a string')
-//         .set({Authorization: `Bearer ${this.tempToken}`})
-//         .end((err, res) => {
-//           expect(res.status).to.equal(400);
-//           expect(res.text).to.equal('SyntaxError');
-//           done();
-//         });
-//       });
-//     });
-//
-//     describe('Testing POST with NON-MATCHING hospitalID and VALID BODY', function() {
-//
-//       before(done => mockUser.call(this, done));
-//       before(done => mockHospital.call(this, done));
-//
-//       it('Should return a status of 404 and an error message', done => {
-//         request.post(`${url}/api/hospital/1234/profile`)
-//         .send({
-//           profileName: exampleProfile.profileName,
-//           userID: `${this.tempUser._id}`,
-//           bio: exampleProfile.bio,
-//           hospitalID: `${this.tempHospital._id}`,
-//         })
-//         .set({Authorization: `Bearer ${this.tempToken}`})
-//         .end((err, res) => {
-//           expect(res.status).to.equal(404);
-//           expect(res.text).to.equal('NotFoundError');
-//           done();
-//         });
-//       });
-//     });
-//
-//     describe('Testing POST with MISSING hospitalID and VALID BODY', function() {
-//
-//       before(done => mockUser.call(this, done));
-//       before(done => mockHospital.call(this, done));
-//
-//       it('Should return a status of 404 and an error message', done => {
-//         request.post(`${url}/api/hospital/1234/profile`)
-//         .send({
-//           profileName: exampleProfile.profileName,
-//           userID: `${this.tempUser._id}`,
-//           bio: exampleProfile.bio,
-//           hospitalID: '1234',
-//         })
-//         .set({Authorization: `Bearer ${this.tempToken}`})
-//         .end((err, res) => {
-//           expect(res.status).to.equal(404);
-//           expect(res.text).to.equal('NotFoundError');
-//           done();
-//         });
-//       });
-//     });
-//
-//   });
-//
-//
 //   describe('Testing GET /api/hospital/:hospitalID/profile/:profileID', function() {
 //
 //     describe('Testing GET with VALID hospitalID and VALID profileID', function() {
